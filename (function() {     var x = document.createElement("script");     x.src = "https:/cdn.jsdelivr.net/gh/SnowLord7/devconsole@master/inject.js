3e52400555c72e21030287c2af817d4bd78594d8
ccsmp13 <60637746+ccsmp13@users.noreply.github.com>
2020-02-04 12:41:25 -0600

    Create inject.js

---
+++ (function() {     var x = document.createElement("script");     x.src = "https:/cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/inject.js
@@ -0,0 +1,6 @@
+(function() {
+    var x = document.createElement("script");
+    x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
+    x.onload = alert("Loaded Developer Console!");
+    document.head.appendChild(x);
+})()
